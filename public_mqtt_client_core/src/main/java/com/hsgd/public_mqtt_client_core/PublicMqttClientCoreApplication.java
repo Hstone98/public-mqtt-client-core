f7a30e9c8603d9cc630bb8de49f12b3c33b643f6
Hstone98 <askhs0302.dev@gmail.com>
2025-11-17 20:40:37 +0900

    Start Project.
    - Project : Maven
    - Language : Java
    - Spring Boot Version : 3.5.7
    - Package Name : com.hsgd.public_mqtt_client_core
    - Packaging : Jar
    - Configuration : YAML
    - Java Version : 21

---
+++ public_mqtt_client_core/src/main/java/com/hsgd/public_mqtt_client_core/PublicMqttClientCoreApplication.java
@@ -0,0 +1,13 @@
+package com.hsgd.public_mqtt_client_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicMqttClientCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicMqttClientCoreApplication.class, args);
+	}
+
+}
---
+++ public_mqtt_client_core/src/test/java/com/hsgd/public_mqtt_client_core/PublicMqttClientCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsgd.public_mqtt_client_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicMqttClientCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
